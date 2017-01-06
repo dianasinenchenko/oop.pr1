abe4121e7a057b96239710052da4245b02954123
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 14:47:38 +0200

    rate workHours

--- src/main/java/school/lemon/changerequest/java/IAccountantImpl.java
+++ src/main/java/school/lemon/changerequest/java/IAccountantImpl.java
@@ -4,43 +4,51 @@ package school.lemon.changerequest.java;
  * Created by Diana on 05.01.2017.
  */
 public class IAccountantImpl implements IAccountant {
-    private int workHoursAccountant;
-    private int rateAccountant;
+    private int rate;
+    private int workHours;
+
 
     public IAccountantImpl(int workHours, int rate) {
-        this.workHoursAccountant = workHours;
-        this.rateAccountant = rate;
+        this.workHours = workHours;
+        this.rate = rate;
+
+
     }
 
-    @Override
-    public int CalculateCurrentMonthSalary() {
-        return 0;
-    }
-
-    @Override
-    public int GetRatioOfWorkedHours() {
-        return 0;
-    }
 
     @Override
     public int GetWorkHours() {
-        return workHoursAccountant;
+        return workHours;
     }
 
 
     @Override
     public void SetWorkHours(int workHours) {
-        this.workHoursAccountant = workHours;
+        this.workHours = workHours;
     }
 
     @Override
     public int GetRate() {
-        return rateAccountant;
+        return rate;
     }
 
 
     @Override
     public void SetRate(int rate) {
-        this.rateAccountant = rate;
+        this.rate = rate;
+    }
+
+    public static int CalculateCurrentMonthSalaryForAccountant(int workHoursAccountant, int rateAccountant) {
+        return (rateAccountant * workHoursAccountant) / 160;
+    }
+
+    public static int CalculateCurrentMonthSalary(int workHours, int rate) {
+
+        return (rate * workHours) / 160;
+    }
+
+
+    static int GetRatioOfWorkedHours() {
+        return 0;
     }
 }
