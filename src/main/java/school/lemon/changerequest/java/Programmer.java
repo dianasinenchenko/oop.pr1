9e8d8a268c92414b60ca9ae0abe9771961374577
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 15:11:50 +0200

    rate workHours

--- src/main/java/school/lemon/changerequest/java/Programmer.java
+++ src/main/java/school/lemon/changerequest/java/Programmer.java
@@ -4,33 +4,29 @@ package school.lemon.changerequest.java;
  * Created by Diana on 06.01.2017.
  */
 public class Programmer extends IAccountantImpl {
-    private int workHoursProgrammer;
-    private int rateProgrammer;
 
     public Programmer(int workHours, int rate) {
         super(workHours, rate);
-        this.workHoursProgrammer = workHours;
-        this.rateProgrammer = rate;
     }
 
     @Override
     public int GetWorkHours() {
-        return workHoursProgrammer;
+        return super.GetWorkHours();
     }
 
     @Override
     public void SetWorkHours(int workHours) {
-        this.workHoursProgrammer = workHours;
+        super.SetWorkHours(workHours);
     }
 
     @Override
     public int GetRate() {
-        return rateProgrammer;
+        return super.GetRate();
     }
 
     @Override
     public void SetRate(int rate) {
-        this.rateProgrammer = rate;
+        super.SetRate(rate);
     }
 
 
