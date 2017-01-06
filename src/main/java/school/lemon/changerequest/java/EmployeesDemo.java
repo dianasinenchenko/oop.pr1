8ec40a34a2d89d6ae130464631d16749fa12ac0e
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 16:11:46 +0200

    Demo

--- src/main/java/school/lemon/changerequest/java/EmployeesDemo.java
+++ src/main/java/school/lemon/changerequest/java/EmployeesDemo.java
@@ -5,23 +5,25 @@ package school.lemon.changerequest.java;
  */
 public class EmployeesDemo {
     public static void main(String[] args) {
-        IAccountantImpl accountant1 = new IAccountantImpl(80, 100);
-        IAccountantImpl accountant2 = new IAccountantImpl(120, 100);
-        IAccountantImpl accountant3 = new IAccountantImpl(165, 100);
+        IAccountantImpl accountant1 = new IAccountantImpl(80, 100,"Olya");
+        IAccountantImpl accountant2 = new IAccountantImpl(120, 100,"Olya");
+        IAccountantImpl accountant3 = new IAccountantImpl(165, 100,"Olya");
 
-        Programmer programmer1 = new Programmer(180, 100);
-        Programmer programmer2 = new Programmer(320, 100);
-        Programmer programmer3 = new Programmer(165, 100);
+        Programmer programmer1 = new Programmer(180, 100,"Diana");
+        Programmer programmer2 = new Programmer(320, 100,"Di");
+        Programmer programmer3 = new Programmer(165, 100,"Dayana");
 
-        Manager manager1 = new Manager(80, 100);
-        Manager manager2 = new Manager(120, 100);
-        Manager manager3 = new Manager(165, 100);
+        Manager manager1 = new Manager(80, 100,"Dima");
+        Manager manager2 = new Manager(120, 100,"Sasha");
+        Manager manager3 = new Manager(165, 100,"Pasha");
 
 
         IAccountantImpl.CalculateCurrentMonthSalary(programmer1.GetWorkHours(), programmer1.GetRate());
         IAccountantImpl.CalculateCurrentMonthSalary(accountant1.GetWorkHours(), accountant1.GetRate());
         IAccountantImpl.CalculateCurrentMonthSalary(manager1.GetWorkHours(), manager1.GetRate());
 
-        System.out.println("Programmer1 workHours -> " + programmer1.GetWorkHours() + " rate -> " + programmer1.GetRate() + " salary -> " + IAccountantImpl.CalculateCurrentMonthSalary(programmer1.GetWorkHours(), programmer1.GetRate()));
+        System.out.println("name -> "  + manager2.GetName() +   "    workHours -> " + manager2.GetWorkHours() + "    rate -> " + manager2.GetRate() + "    salary -> " + IAccountantImpl.CalculateCurrentMonthSalary(manager2.GetWorkHours(), manager2.GetRate()));
+
+        System.out.println("name -> " + programmer2.GetName()+  "    workHours -> " + programmer2.GetWorkHours() + "    rate -> " + programmer2.GetRate() + "    salary -> " + IAccountantImpl.CalculateCurrentMonthSalary(programmer2.GetWorkHours(), programmer2.GetRate()));
     }
 }
