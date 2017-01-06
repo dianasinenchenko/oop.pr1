465291ad4927526dd32148d98d9b4c60282961ac
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 12:39:09 +0200

    Employees

---
+++ src/main/java/school/lemon/changerequest/java/Employees.java
@@ -0,0 +1,18 @@
+package school.lemon.changerequest.java;
+
+/**
+ * Created by Diana on 05.01.2017.
+ */
+public interface Employees {
+    int CalculateCurrentMonthSalary();
+
+    int GetRatioOfWorkedHours();
+
+    int GetWorkHours();
+
+    void SetWorkHours(int workHours);
+
+    int GetRate();
+
+    void SetRate(int rate);
+}
