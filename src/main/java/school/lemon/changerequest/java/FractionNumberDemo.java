908236aeffa841dfe17abd90d7a50522380d4221
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 12:13:58 +0200

    Demo all

--- src/main/java/school/lemon/changerequest/java/FractionNumberDemo.java
+++ src/main/java/school/lemon/changerequest/java/FractionNumberDemo.java
@@ -12,6 +12,7 @@ public class FractionNumberDemo {
         FractionNumber fractionNumber5 = new FractionNumber(-9, -3);
         FractionNumber fractionNumber6 = new FractionNumber(-9, 3);
         FractionNumber fractionNumber7 = new FractionNumber(0, 3);
+
         System.out.println("dividend is 3 and divisor is 9 = " + fractionNumber1.ToString());
         System.out.println("dividend is 9 and divisor is 9 = " + fractionNumber2.ToString());
         System.out.println("dividend is 9 and divisor is 3 = " + fractionNumber3.ToString());
@@ -19,11 +20,12 @@ public class FractionNumberDemo {
         System.out.println("dividend is -9 and divisor is -3 = " + fractionNumber5.ToString());
         System.out.println("dividend is -9 and divisor is 3 = " + fractionNumber6.ToString());
         System.out.println("dividend is 0 and divisor is 3 = " + fractionNumber7.ToString());
-        System.out.println("add "+fractionNumber1.ToString() +" and "+ fractionNumber2.ToString() + " is "+ fractionNumber1.AddAnotherFractionNumber(fractionNumber2).ToString());
-        System.out.println("subtract "+fractionNumber1.ToString() +" and "+ fractionNumber2.ToString() + " is "+ fractionNumber1.SubtractAnotherFractionNumber(fractionNumber2).ToString());
-        System.out.println("multiply "+fractionNumber1.ToString() +" and "+ fractionNumber2.ToString() + " is "+ fractionNumber1.MultiplyAnotherFractionNumber(fractionNumber2).ToString());
-        System.out.println("divide "+fractionNumber1.ToString() +" and "+ fractionNumber2.ToString() + " is "+ fractionNumber1.DivideAnotherFractionNumber(fractionNumber2).ToString());
-        System.out.println("ONE "+ FractionNumber.ONE.ToString());
-        System.out.println("ZERRO "+ FractionNumber.ZERRo.ToString());
+        System.out.println("add " + fractionNumber1.ToString() + " and " + fractionNumber2.ToString() + " is " + fractionNumber1.GetDecimalValue());
+        System.out.println("add " + fractionNumber1.ToString() + " and " + fractionNumber2.ToString() + " is " + fractionNumber1.AddAnotherFractionNumber(fractionNumber2).ToString());
+        System.out.println("subtract " + fractionNumber1.ToString() + " and " + fractionNumber2.ToString() + " is " + fractionNumber1.SubtractAnotherFractionNumber(fractionNumber2).ToString());
+        System.out.println("multiply " + fractionNumber1.ToString() + " and " + fractionNumber2.ToString() + " is " + fractionNumber1.MultiplyAnotherFractionNumber(fractionNumber2).ToString());
+        System.out.println("divide " + fractionNumber1.ToString() + " and " + fractionNumber2.ToString() + " is " + fractionNumber1.DivideAnotherFractionNumber(fractionNumber2).ToString());
+        System.out.println("ONE " + FractionNumber.ONE.ToString());
+        System.out.println("ZERRO " + FractionNumber.ZERRo.ToString());
     }
 }
