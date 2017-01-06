62436f1cfff699383e6915c23bbfb607c86c08f2
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-06 16:13:16 +0200

    name

--- src/main/java/school/lemon/changerequest/java/EmployeesCreate.java
+++ src/main/java/school/lemon/changerequest/java/EmployeesCreate.java
@@ -4,20 +4,20 @@ package school.lemon.changerequest.java;
  * Created by Diana on 06.01.2017.
  */
 public final class EmployeesCreate {
-    public static IAccountant createAccountant(int workHours, int rate) {
-        IAccountantImpl accountant = new IAccountantImpl(workHours, rate);
+    public static IAccountant createAccountant(int workHours, int rate, String name) {
+        IAccountantImpl accountant = new IAccountantImpl(workHours, rate, name);
         return accountant;
     }
 
 
-    public static IAccountant createProgrammer(int workHours, int rate) {
-        Programmer programmer = new Programmer(workHours, rate);
+    public static IAccountant createProgrammer(int workHours, int rate, String name) {
+        Programmer programmer = new Programmer(workHours, rate, name);
         return programmer;
     }
 
 
-    public static IAccountant createManager(int workHours, int rate) {
-        Manager manager = new Manager(workHours, rate);
+    public static IAccountant createManager(int workHours, int rate, String name) {
+        Manager manager = new Manager(workHours, rate, name);
         return manager;
     }
 }
